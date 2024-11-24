e88e17908849484f148d3edac50ffc5aaf72083f
DootLord <shaunoman840@gmail.com>
2024-11-24 17:52:52 +0000

    Tidied up lack of string trim on returned sheets

--- helper/sheet.helper.ts
+++ helper/sheet.helper.ts
@@ -2,20 +2,16 @@ import { ISheetData, ISheetDataDB, sheetDataSchema } from "../schema/sheet-schem
 
 class SheetHelper {
     mergeSheet(sheetDataDB: ISheetDataDB): ISheetData { //? This could live in a helper file
-        const sheetData: ISheetData = {
-            id: sheetDataDB.id,
-            name: sheetDataDB.name,
-            jobs: sheetDataDB.jobs.split(','),
-            monday: sheetDataDB.monday.split(','),
-            tuesday: sheetDataDB.tuesday.split(','),
-            wednesday: sheetDataDB.wednesday.split(','),
-            thursday: sheetDataDB.thursday.split(','),
-            friday: sheetDataDB.friday.split(','),
-            saturday: sheetDataDB.saturday.split(','),
-            sunday: sheetDataDB.sunday.split(','),
+        const arrayItems = ['jobs', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+        const removeQuotesRegex = /['"\\]/g; // This removes quotes AND backslashes from the string
+
+        for (const item of arrayItems) {
+            if (sheetDataDB[item]) {
+                sheetDataDB[item] = sheetDataDB[item].replace(removeQuotesRegex, '').split(',').map((item: string) => item.trim());
+            }
         }
 
-        const parse = sheetDataSchema.safeParse(sheetData);
+        const parse = sheetDataSchema.safeParse(sheetDataDB);
 
         if (!parse.success) {
             throw new Error(parse.error.message);
